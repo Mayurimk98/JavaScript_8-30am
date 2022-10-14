e60470fac1e7d97f1971b6197b82ed182b3458ce
Mayurimk98 <mayurimkatwe@gmail.com>
2022-10-14 08:22:23 +0530

    13_10_whileLoop

---
+++ whileLoop13_10.js
@@ -0,0 +1,59 @@
+
+//For loop => duration
+
+//while loop => we dont know duration
+
+//while loop syntax
+//initialized
+//while(condition check){
+//statements
+
+
+//increment/decremenet
+//}
+
+//1-5 print
+// let i = 1  //1
+// while (i <= 5) { //1<=5 2<=5  3<=5 4<=5 5<=5  6<=5
+//     console.log(i) //1  2  3  4 5
+
+//     i++ //2 3 4 5 6
+// }
+
+// //welcome = 10 time
+// let j = 0
+// while (j < 10) {
+//     console.log('welcome!!')
+//     j++
+// }
+
+//reverse direction 
+//10 -1
+// let k = 10
+// while (k >= 1) {
+//     console.log(k)
+//     k--
+// }
+
+//break
+// let l = 0
+// while (l < 6) {  // 0<6  1<6 2<6  3<6
+//     if (l == 3) { //0==3  1==3  2==3  3==3
+//         break
+//     }
+//     console.log(l) //0 1  2
+//     l++ //1  2  3
+// }
+// console.log('===============================')
+
+// let i1 = 0
+// while (i1 < 5) { //0<5  1<5  2<5  3<5
+//     console.log(i1)  //0  1  2  3
+//     if (i1 == 3) {  //0==3  1==3  2==3  3==3
+//         break
+//     }
+//     i1++  //1 2  3
+// }
+
+//continue
+
